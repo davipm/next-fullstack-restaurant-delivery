b6db6ff6a1dc7a69de516c951b4d2422819d73ee
davi_mendoca <davi.p.m94@gmail.com>
2023-09-07 21:01:53 -0300

    update app

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,13 +1,58 @@
-'use client'
+"use client";
 
-import { useState } from "react";
+import Image from "next/image";
+import { useState, useEffect } from "react";
+
+const data = [
+  {
+    id: 1,
+    title: "always fresh & always crispy & always hot",
+    image: "/slide1.png",
+  },
+  {
+    id: 2,
+    title: "we deliver your order wherever you are in NY",
+    image: "/slide2.png",
+  },
+  {
+    id: 3,
+    title: "the best pizza to share with your family",
+    image: "/slide3.jpg",
+  },
+];
 
 export default function Slider() {
-  const [item, setItem] = useState(null);
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(
+      () =>
+        setCurrentSlide((prevState) =>
+          prevState === data.length - 1 ? 0 : prevState + 1,
+        ),
+      4000,
+    );
+    return () => clearInterval(interval);
+  }, []);
 
   return (
-    <div>
-      <p>Slider</p>
+    <div className="flex flex-col h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] lg:flex-row bg-fuchsia-50">
+      <div className="flex-1 flex items-center justify-center flex-col gap-8 text-red-500 font-bold">
+        <h1 className="text-5xl text-center uppercase p-4 md:p-10 md:text-6xl xl:text-7xl">
+          {data[currentSlide].title}
+        </h1>
+        <button className="bg-red-500 text-white py-4 px-8">Order Now</button>
+      </div>
+
+      <div className="w-full flex-1 relative">
+        <Image
+          src={data[currentSlide].image}
+          alt={data[currentSlide].title}
+          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+          fill
+          className="object-cover"
+        />
+      </div>
     </div>
-  )
+  );
 }
