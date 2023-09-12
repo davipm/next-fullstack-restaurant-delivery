23742273bae761a1f7abe7b093abc3f36dadfc28
davi_mendoca <davi.p.m94@gmail.com>
2023-09-11 23:28:32 -0300

    adding devtools

--- src/utils/store.ts
+++ src/utils/store.ts
@@ -1,5 +1,5 @@
 import { create } from "zustand";
-import { persist } from "zustand/middleware";
+import { persist, devtools } from "zustand/middleware";
 
 import { ActionTypes, CartType } from "@/@types";
 
@@ -10,45 +10,47 @@ const INITIAL_STATE = {
 };
 
 export const useCartStore = create(
-  persist<CartType & ActionTypes>(
-    (set, get) => ({
-      products: INITIAL_STATE.products,
-      totalItems: INITIAL_STATE.totalItems,
-      totalPrice: INITIAL_STATE.totalPrice,
+  devtools(
+    persist<CartType & ActionTypes>(
+      (set, get) => ({
+        products: INITIAL_STATE.products,
+        totalItems: INITIAL_STATE.totalItems,
+        totalPrice: INITIAL_STATE.totalPrice,
 
-      addToCart(item) {
-        const products = get().products;
-        const productInState = products.find(
-          (product) => product.id === item.id,
-        );
-
-        if (productInState) {
-          const updatedProducts = products.map((product) =>
-            product.id === productInState.id
-              ? {
-                  ...item,
-                  quantity: item.quantity + product.quantity,
-                  price: item.price + product.price,
-                }
-              : item,
+        addToCart(item) {
+          const products = get().products;
+          const productInState = products.find(
+            (product) => product.id === item.id,
           );
 
-          set((state) => ({
-            products: updatedProducts,
-            totalItems: state.totalItems + item.quantity,
-            totalPrice: state.totalPrice + item.price,
-          }));
-        } else {
-          set((state) => ({
-            products: [...state.products, item],
-            totalItems: state.totalItems + item.quantity,
-            totalPrice: state.totalPrice + item.price,
-          }));
-        }
-      },
+          if (productInState) {
+            const updatedProducts = products.map((product) =>
+              product.id === productInState.id
+                ? {
+                    ...item,
+                    quantity: item.quantity + product.quantity,
+                    price: item.price + product.price,
+                  }
+                : item,
+            );
 
-      removeFromCart(item) {},
-    }),
-    { name: "cart", skipHydration: true },
+            set((state) => ({
+              products: updatedProducts,
+              totalItems: state.totalItems + item.quantity,
+              totalPrice: state.totalPrice + item.price,
+            }));
+          } else {
+            set((state) => ({
+              products: [...state.products, item],
+              totalItems: state.totalItems + item.quantity,
+              totalPrice: state.totalPrice + item.price,
+            }));
+          }
+        },
+
+        removeFromCart(item) {},
+      }),
+      { name: "cart", skipHydration: true },
+    ),
   ),
 );
