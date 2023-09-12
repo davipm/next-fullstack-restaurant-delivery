2c2dc6346d322060f2b69a2376247c8ef2d10274
davi_mendoca <davi.p.m94@gmail.com>
2023-09-11 23:20:39 -0300

    create store

--- src/@types/index.ts
+++ src/@types/index.ts
@@ -31,3 +31,23 @@ export type Product = {
   price: number;
   options?: { title: string; additionalPrice: number }[];
 };
+
+export type CartItemType = {
+  id: number;
+  title: string;
+  img?: string;
+  price: number;
+  optionTitle?: string;
+  quantity: number;
+};
+
+export type CartType = {
+  products: CartItemType[];
+  totalItems: number;
+  totalPrice: number;
+};
+
+export type ActionTypes = {
+  addToCart: (item: CartItemType) => void;
+  removeFromCart: (item: CartItemType) => void;
+};
