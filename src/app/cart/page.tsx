501b0eec9f14c6eda789b46080ec145c76856a3a
davi_mendoca <davi.p.m94@gmail.com>
2023-09-13 01:13:50 -0300

    create cart page and init pay id page

---
+++ src/app/cart/page.tsx
@@ -0,0 +1,132 @@
+"use client";
+
+import Image from "next/image";
+import { useEffect } from "react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { useMutation } from "@tanstack/react-query";
+import { toast } from "react-toastify";
+
+import api from "@/utils/service";
+import { useCartStore } from "@/utils/store";
+import { CartItemType } from "@/@types";
+
+interface Props {
+  products: CartItemType[];
+  price: number;
+  userEmail: string | null | undefined;
+}
+
+async function makeCheckoutPost({ products, price, userEmail }: Props) {
+  const headers = { "Content-Type": "application/json" };
+  const body = {
+    products,
+    price,
+    userEmail,
+    status: "Not Paid",
+  };
+
+  try {
+    const { data } = await api.post<{ id: string }>("/orders", body, {
+      headers,
+    });
+    return data;
+  } catch (error) {
+    throw new Error("Got a error while try create checkout");
+  }
+}
+
+export default function Page() {
+  const { products, totalItems, totalPrice, removeFromCart } = useCartStore();
+  const { data: session } = useSession();
+  const router = useRouter();
+
+  const mutation = useMutation({
+    mutationFn: () =>
+      makeCheckoutPost({
+        products,
+        price: totalPrice,
+        userEmail: session?.user.email,
+      }),
+    onSuccess: ({ id }) => {
+      toast("The product has been deleted!");
+      router.push(`/pay/${id}`);
+    },
+    onError: () => {
+      toast.error("Error creating checkout ");
+    },
+  });
+
+  const handleCheckout = () => {
+    if (!session) router.push(`/login`);
+    else mutation.mutate();
+  };
+
+  useEffect(() => {
+    useCartStore.persist.rehydrate();
+  }, []);
+
+  return (
+    <section className="h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] flex flex-col text-red-500 lg:flex-row">
+      <div className="h-1/2 p-4 flex flex-col justify-center overflow-auto lg:h-full lg:w-2/3 2xl:w-1/2 lg:px-20 xl:px-40">
+        {products.map((product) => (
+          <div
+            key={product.id}
+            className="flex items-center justify-between mb-4"
+          >
+            {product.img && (
+              <Image
+                src={product.img}
+                alt={product.title}
+                width={100}
+                height={100}
+              />
+            )}
+
+            <div>
+              <h1 className="uppercase text-xl font-bold">
+                {product.title} x{product.quantity}
+              </h1>
+              <span>{product.optionTitle}</span>
+            </div>
+
+            <h2 className="font-bold">{product.price}</h2>
+
+            <span
+              className="cursor-pointer"
+              onClick={() => removeFromCart(product)}
+            >
+              X
+            </span>
+          </div>
+        ))}
+      </div>
+      <div className="h-1/2 p-4 bg-fuchsia-50 flex flex-col gap-4 justify-center lg:h-full lg:w-1/3 2xl:w-1/2 lg:px-20 xl:px-40 2xl:text-xl 2xl:gap-6">
+        <div className="flex justify-between">
+          <span>Subtotal ({totalItems} items)</span>
+          <span>${totalItems}</span>
+        </div>
+        <div className="flex justify-between">
+          <span>Service Cost</span>
+          <span>$0.00</span>
+        </div>
+        <div className="flex justify-between">
+          <span>Delivery Cost</span>
+          <span className="text-green-500">FREE!</span>
+        </div>
+        <hr className="my-2" />
+        <div className="flex justify-between">
+          <span>TOTAL(INCL. VAT)</span>
+          <span className="font-bold">{totalPrice}</span>
+        </div>
+
+        <button
+          className="bg-red-500 text-white p-3 rounded-md w-1/2 self-end"
+          onClick={handleCheckout}
+        >
+          CHECKOUT
+        </button>
+      </div>
+    </section>
+  );
+}
--- src/components/CartIcon.tsx
+++ src/components/CartIcon.tsx
@@ -2,17 +2,25 @@
 
 import Link from "next/link";
 import Image from "next/image";
+import { useEffect } from "react";
 import { useSession } from "next-auth/react";
 
+import { useCartStore } from "@/utils/store";
+
 export default function CartIcon() {
+  const { totalItems } = useCartStore();
   const { data: session } = useSession();
+
+  useEffect(() => {
+    useCartStore.persist.rehydrate();
+  }, []);
 
   return (
     <div className="flex items-center gap-4">
       <div className="relative w-8 h-8 md:w-5 md:h-5">
         <Image
           src="/cart.png"
-          alt=""
+          alt="cart"
           fill
           sizes="100%"
           className="object-contain"
@@ -25,7 +33,7 @@ export default function CartIcon() {
         </Link>
       )}
 
-      <Link href="/cart">Cart ({0})</Link>
+      <Link href="/cart">Cart ({totalItems})</Link>
     </div>
   );
 }
