7668187c6316f75501f15079d9db5ba2333d8e6a
Davi Pereira <davi.p.m94@gmail.com>
2023-09-15 18:40:50 -0300

    init order page

--- src/app/orders/page.tsx
+++ src/app/orders/page.tsx
@@ -2,15 +2,21 @@
 
 import { useSession } from "next-auth/react";
 import { redirect } from "next/navigation";
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import api from "@/utils/service";
 import { OrderType } from "@/@types";
 import LoadingSpinner from "@/components/Loading";
 import { formatToMoney } from "@/utils/helpers";
 import cx from "classnames";
+import Image from "next/image";
+import { FormEvent, useState } from "react";
+import { toast } from "react-toastify";
 
 export default function Orders() {
+  const queryClient = useQueryClient();
   const { data: session, status } = useSession();
+
+  const [orderStatus, setOrderStatus] = useState("");
 
   const { data: orders, isLoading } = useQuery({
     queryKey: ["orders"],
@@ -24,12 +30,31 @@ export default function Orders() {
     },
   });
 
+  const mutation = useMutation({
+    mutationFn: ({ id, status }: { id: string; status: string }) => {
+      const headers = { "Content-Type": "application/json" };
+      return api.put(`/orders/${id}`, { body: status }, { headers });
+    },
+    onSuccess: async () => {
+      await queryClient.invalidateQueries({ queryKey: ["orders"] });
+      toast.success("The order status has been changed!");
+    },
+    onError: () => {
+      toast.error("Error trying to update Order!");
+    },
+  });
+
+  function handleSubmit(e: FormEvent<HTMLFormElement>, id: string) {
+    e.preventDefault();
+    mutation.mutate({ id, status: orderStatus });
+  }
+
   if (status === "unauthenticated") redirect("/");
 
   if (isLoading) return <LoadingSpinner />;
 
   return (
-    <div className="p-4 lg:px-20 xl:px-40">
+    <section className="min-h-[calc(70vh)] p-4 lg:px-20 xl:px-40">
       <table className="w-full border-separate border-spacing-3">
         <thead>
           <tr className="text-left">
@@ -54,11 +79,40 @@ export default function Orders() {
               <td className="hidden md:block py-6 px-1">
                 {order.products[0].title}
               </td>
-              {/* TODO: create admin section */}
+              {session?.user.isAdmin ? (
+                <td>
+                  <form
+                    className="flex items-center justify-center gap-4"
+                    onSubmit={(event) => handleSubmit(event, order.id)}
+                  >
+                    <input
+                      type="text"
+                      className="p-2 ring-1 ring-blue-100 rounded-md"
+                      placeholder={order.status}
+                      value={orderStatus}
+                      onChange={(event) => setOrderStatus(event.target.value)}
+                    />
+
+                    <button
+                      type="submit"
+                      className="bg-red-400 p-2 rounded-full"
+                    >
+                      <Image
+                        src="/edit.png"
+                        alt="Edit"
+                        width={20}
+                        height={20}
+                      />
+                    </button>
+                  </form>
+                </td>
+              ) : (
+                <td className="py-6 px-1">{order.status}</td>
+              )}
             </tr>
           ))}
         </tbody>
       </table>
-    </div>
+    </section>
   );
 }
