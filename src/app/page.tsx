d6cdc807869e4c5a04713ef441354098edcc3ec3
Davi Pereira <davi.p.m94@gmail.com>
2023-09-18 13:59:06 -0300

    some updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,10 @@ import Featured from "@/components/Featured";
 import Offer from "@/components/Offer";
 import Slider from "@/components/Slider";
 
+export const dynamic = "force-dynamic";
+export const dynamicParams = true;
+export const revalidate = 0;
+
 export default function Home() {
   return (
     <main>
