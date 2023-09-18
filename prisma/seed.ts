e29038cb5e830b8a0a8bb49f54d02f73c055d8c4
Davi Pereira <davi.p.m94@gmail.com>
2023-09-18 12:12:04 -0300

    adding seed

---
+++ prisma/seed.ts
@@ -0,0 +1,16 @@
+import prisma from "@/utils/connect";
+
+async function main() {
+  const response = await Promise.all([]);
+  console.log(response);
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
